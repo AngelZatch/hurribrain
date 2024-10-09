f796895bf9013f8e6b6a91a6253d83f033522462
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2024-10-09 08:07:09 +0000

    feat: getting fastify server running

---
+++ packages/api/src/index.ts
@@ -0,0 +1,20 @@
+import Fastify from 'fastify';
+
+const server = Fastify();
+
+server.get('/', async () => {
+  return { hello: 'world' };
+});
+
+const start = async () => {
+  try {
+	await server.listen({ port: 8080 });
+	console.log(`Server is running at http://localhost:8080`);
+  } catch (err) {
+    console.error(err);
+	server.log.error(err);
+	process.exit(1);
+  }
+};
+
+start();
