8fca1f6771e2ab1f3a6fdb71386d641cd974db3f
AngelZatch <angelzatch@gmail.com>
2025-01-02 19:29:42 +0100

    feat: shell game screen

--- packages/app/app/(app)/(games)/play/[id].tsx
+++ packages/app/app/(app)/(games)/play/[id].tsx
@@ -5,8 +5,11 @@ import React from "react";
 import { View, Text } from "react-native";
 import { PageContainer } from "@/components/ui/PageContainer";
 import TopNavigation from "@/components/TopNavigation";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function PlayScreen() {
+  const colorScheme = useColorScheme();
   const { user } = useAuth();
   const { id: gameId } = useLocalSearchParams<{ id: string }>();
 
@@ -25,7 +28,20 @@ export default function PlayScreen() {
   return (
     <PageContainer>
       <TopNavigation topLabel="Game code:" subLabel={data?.code} />
-      <View>
+      <View
+        style={{
+          padding: 8,
+          flex: 1,
+          width: "100%",
+          backgroundColor: Colors[colorScheme ?? "light"].containerBackground,
+          borderRadius: 20,
+          borderWidth: 1,
+          borderStyle: "solid",
+          backdropFilter: "blur(5px)",
+          borderColor:
+            "linear-gradient(45deg, rgba(255, 255, 255, 1) 0%, rgba(218, 191, 224, 0.1) 33%, rgba(176, 130, 193, 0.3) 67%, rgba(176, 130, 193, 0.5) 100%)",
+        }}
+      >
         <Text>Play Screen</Text>
       </View>
     </PageContainer>
