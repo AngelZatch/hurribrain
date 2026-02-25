de4440955e9fe44df9cf75b6f34afee0278e9aeb
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2026-02-25 08:19:19 +0100

    feat: UI to lead to delete account modal

--- packages/app/app/(app)/settings/security.tsx
+++ packages/app/app/(app)/settings/security.tsx
@@ -4,6 +4,7 @@ import { PageContainer } from "@/components/ui/PageContainer";
 import ThemedButton from "@/components/ui/ThemedButton";
 import ThemedText from "@/components/ui/ThemedText";
 import { Link } from "expo-router";
+import { View } from "react-native";
 
 export default function SecurityAndConnectionScreen() {
   return (
@@ -25,9 +26,19 @@ export default function SecurityAndConnectionScreen() {
       <ContainerView
         style={{
           flexDirection: "column",
+          flex: 1,
         }}
       >
-        <ThemedText>Coucou</ThemedText>
+        <ThemedText type="settingsSectionTitle">Zone Dangereuse</ThemedText>
+        <View>
+          <ThemedText type="label">Suppression de Compte</ThemedText>
+          <ThemedText type="helper">
+            Supprime définitivement ton compte et toutes les données associées.
+            Tu seras retiré.e de toutes les parties en cours. Cette action est
+            irréversible.
+          </ThemedText>
+          <ThemedButton title="Suppression de compte" fullWidth type="danger" />
+        </View>
       </ContainerView>
     </PageContainer>
   );
