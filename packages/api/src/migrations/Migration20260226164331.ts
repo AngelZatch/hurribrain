c5ffa747e23bcf80e5c06178d68b35feb846e503
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2026-02-27 04:33:10 +0100

    fix: lint

--- packages/api/src/migrations/Migration20260226164331.ts
+++ packages/api/src/migrations/Migration20260226164331.ts
@@ -1,13 +1,13 @@
-import { Migration } from '@mikro-orm/migrations';
+import { Migration } from "@mikro-orm/migrations"
 
 export class Migration20260226164331 extends Migration {
-
   override async up(): Promise<void> {
-    this.addSql(`alter table \`turn\` add \`is_gold\` tinyint(1) not null default false;`);
+    this.addSql(
+      `alter table \`turn\` add \`is_gold\` tinyint(1) not null default false;`
+    )
   }
 
   override async down(): Promise<void> {
-    this.addSql(`alter table \`turn\` drop column \`is_gold\`;`);
+    this.addSql(`alter table \`turn\` drop column \`is_gold\`;`)
   }
-
 }
